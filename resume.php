13cbb7671866cf04ece855bff7552b0405a21b58
Oclinton <omarclinton@gmail.com>
2020-09-29 11:32:38 -0400

    This Commit

---
+++ resume.php
@@ -0,0 +1,424 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>Omar Clinton Resume</title>
+<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Oxygen">
+<link rel ="stylesheet" type = "text/css" href = css/resume.css>
+<style type="text/css"></style>
+</head>
+<body>
+
+<!--
+<header id = "headerDiv">
+		<div id = "topBan">
+			<div id = "navContent">
+					<ul>
+						<li><a class="memNavItem" href= "portfolio.php">Portfolio</a></li>
+						<li><a class="first after" href= "resume.php">Resume</a></li>
+						<li><a class="first after" href= "omarabout.php">About Me</a></li>
+						<li><a class="navLink3" href= "Tutorials.php">Tutorials</a></li>
+						<li><a class="navLink3" href= "contact.php">Support</a></li>
+						<li><a class="navLink3" href= "contact.php">Contact</a></li>
+					</ul>
+				</div>
+	</div>
+	<div id = "thinLine"></div>
+		<div id = "headCont">
+			<div id = "navContent"></div>
+				<a href = "index.php"><div id="logoDiv" style="font-family: 'Oxygen', sans-serif">OmarClinton.com</div></a>
+			</div>
+		</div>
+</header>
+-->
+
+	<header id="headerDiv">
+		<div id="topBan">
+			<div id = "navContent">
+				<ul>
+					<li><a class="memNavItem" href= "portfolio.php">Portfolio</a></li>
+					<li><a class="first after" href= "resume.php">Resume</a></li>
+					<li><a class="first after" href= "omarabout.php">About Me</a></li>
+					<li><a class="navLink3" href= "tutorials.php">Tutorials</a></li>
+					<li><a class="navLink3" href= "services.php">Services</a></li>
+					<li><a class="navLink3" href= "contact.php">Contact</a></li>
+				</ul>
+			</div>
+			</div>
+			<div id="thinLine"></div>
+			<div id="headCont">
+					<a href="index.php"><div id="logoDiv" style="font-family: 'Oxygen', sans-serif">OmarClinton.com</div></a>
+			</div>
+		</div>
+	</header>
+	<div id = "extendNav"></div>
+
+<main id = "mainDiv" style="background: #424949">
+		<div id = "memVidMain">
+		<div id = "sampleHeader"></div>
+		<p style = "font-family: sans-serif; color: white;">
+
+<div id = "resume">
+<h2>Omar Clinton's Resume</h2>
+	Phone: 919.610.0665<br>
+	Email: omarclinton@gmail.com<br>
+	GitHub: https://github.com/Oclinton<br>
+	<br>
+	<b>Education</b><br>
+	-Santa Fe College - 2011-2013 Focus: Ballet<br>
+	-Hillsborough Community College - 2009-2011 Focus: Ballet<br>
+	-UNC Chapel Hill, NC 1996-2001 Major: Communication Studies<br>
+	-High School: Aviano American High School, Aviano Italy
+	<br>
+	<br>
+
+	<b>Fitness Certifications</b>
+	-Z-Health R-phase<br>
+	-Z-Health I-Phase<br>
+	-Z-Health S-Phase<br>
+	-Z-Health T-Phase<br>
+	-Z-Health 9S Strength and Suppleness<br>
+	-Z-Health 9S Speed<br>
+	-RKC - Russian Kettlebell Certified
+	<br>
+	<br>
+	<br>
+
+	<b>Other technologies</b><br>
+WordPress, HTML, CSS, Java, JavaScript, Bootstrap Jquery, PHP, AJAX, XML, UX/UI design Intermediate/beginner AngularJS, MySQL, JSON, SQL CRUD (Create Read Update Delete), Bootstrap, Ecommerce Website Development, Payment integration, Membership Systems, Database Management, PHP Mysql integration. Computer architecture?  I understand Docker conceptually. Intermediate understanding of devOps. Android mobile application development. Git. Docker. Jekyll. <br>
+----Learning: React, NodeJS, as well as some other javascript frameworks.<br>
+-I would love an opportunity to understand my level of competency on each of these topics.<br> 
+
+
+	<br>
+	---10+ Years in the Fitness and Wellness Industry<br>
+	---5+ years experience working with Graphic design, UX design  HTML CSS and javascript<br>
+	<br>
+
+	<b>DayLight Data Internship | Jan 2018 - April 2018</b><br>
+	Software Development Intern<br>
+	Projects:<br>
+	-Redesign Neuroplus Blog<br>
+	-Data DayLight Website<br>
+	-Data DayLight Internship Mobile Application Template<br>
+	<br>
+
+	<b>Studio Athletica Human Enhancement | 2011 -2014</b><br>
+	--Personal Trainer / Small Business Owner -- Clinton Consulting LLC.<br>
+	Responsibilities:<br>
+	-Marketing funnel campaign management.<br>
+	-Personal training sales.<br>
+	-Management of the SOP.<br>
+	-Client management, billing, session tracking, and ongoing sales and support.<br>
+	-Social media management.<br>
+	-Management of client physical well being by managing their training.<br>
+	-Was responsible for the sales of my own personal training services.<br>
+	-Created a new program named Ballet Athletique.<br>
+	-Created and choreographed a dance performance that was performed and a dance festival in Jacksonville FL in the spring of 2013<br>
+	-Designed the Studio-Athletica website<br>
+	https://www.facebook.com/studioathletica?ref=bookmarks<br>
+	<br>
+	<br>
+
+	<b>Sprint Dynamics - CNS Based Sprint Speed Enhancement Program | 2009 - 2014</b><br>
+	---Speed Enhancement Coach<br>
+	Responsibilities:<br>
+	-Created a marketing system for this program<br>
+	-Created a website to help with the marketing of the program<br>
+	-Wrote Articles for my weekly weblog.<br>
+	-Helped athletes improve their sprint speed by training their nervous system<br>
+	<br>
+	<br>
+
+	<b>The Mambo Experience | 2008 - 2013</b><br>
+	---Creator/Event Director<br>
+	Responsibilities:<br>
+	-Conceptualize and bring to fruition a social event: The Mambo Experience.<br>
+	-Securing the venue,<br>
+	-Hiring staff, artists and musicians,<br>
+	-Marketing via social events and media, coordinating with vendors,<br>
+	-Managing all components during the event.<br>
+	<br>
+	<br>
+
+	<b>Rapid Results Tampa | 2009 - 2011</b><br>
+	---Owner/Personal Fitness Trainer<br>
+	Responsibilities:<br>
+	-Trained clients in physical exercise.<br>
+	-Taught clients about nutrition and pain management.<br>
+	-Taught group exercise classes, marketed via social events and media.<br>
+	-Designed print media such as business cards and flyers.<br>
+	-Designed website using Adobe Photoshop, Illustrator and Dreamweaver.<br>
+	<br>
+	<br>
+
+	<b>Body Isolations Specialist | 2007 - 2013</b><br>
+	---Performer/Teacher<br>
+	Responsibilities:<br>
+	-Perform ballet and Latin dance routines at social and artistic events.<br>
+	-Teach body isolations and salsa workshops to dancers of all levels.<br>
+	-Taught basic and advanced salsa instruction.<br>
+	<br>
+	<br>
+
+	<b>Triangle Personal Training | 2003 - 2009 </b><br>
+	---Owner/Personal Trainer<br>
+	Responsibilities<br>
+	-Trained clients in physical exercise.<br>
+	-Taught clients about nutrition and nutrition planning.<br>
+	-Management, taught group exercise classes.<br>
+	-Position also entailed marketing via print and social media.<br>
+	-Established and implemented and maintained standard operating procedures.<br>
+	<br>
+	<br>
+
+	<b>Fitness World | 2002-2003</b><br>
+	---Front Desk Employee; Front desk Manager; membership sales; personal trainer.<br>
+	Responsibilities:<br>
+	-Manage part time front desk employees.<br>
+	-Hired and managed front desk employees.<br>
+	-Managed Employee schedules and payroll.<br>
+	-Created online marketing funnel to support in person sales<br>
+	-Design programs for members such as a 5k fun run and basketball tournaments.<br>
+	-Received first certification through ISSA<br>
+	<br>
+	<br>
+
+	<b>Cross Communications | 2001-2001</b><br>
+	---Web designer:<br>
+	Responsibilities<br>
+	-Plan the sitemap of the website.<br>
+	-Meet with clients to understand their needs.<br>
+	-Keep clients updated as to the progress of the project.<br>
+	-Designthumbnails off website concepts<br>
+	-Design Websites in Photoshop<br>
+	-Code the websites designed in Photoshop to HTML HTML<br>
+	<br>
+	<br>
+
+	<b>Fluid Communications | 2000</b><br>
+	---Internship<br>
+	Networking company with a focus on the hospitality industry.<br>
+	Design Flyers<br>
+	<br>
+	<br>
+
+	<b> Skills:</b><br>
+	MS Excel<br>
+	Adobe Creative Suite.<br>
+	<br>
+	<br>
+
+	<b>Programming, markup languages, and computer concepts:</b><br>
+	---HTML<br>
+	---JavaScript<br>
+	---JQuery<br>
+	---AngularJS<br>
+	---Java<br>
+	---PHP<br>
+	---CSS<br>
+	---AJAX<br>
+	---JSON<br>
+	---XML<br>
+	---MySQL<br>
+	---Abstraction of computer programing design<br>
+	<br>
+	<br>
+
+	<b>Things at which I strive:</b><br>
+	To be organized.<br>
+	To value others time.<br>
+	To be driven.<br>
+	To be a self starter.<br>
+	To be friendly.<br>
+	To be personable.<br>
+	To be a team player.<br>
+	To acquire information quickly.<br>
+	To be  and focused.<br>
+	Other Relevant Skills: I have written a book that Is for sale on Amazon:<br>
+	<a href="https://www.amazon.com/dp/B00M98TY1C/ref=dp-kindle-redirect?_encoding=UTF8&btkr=1" style="color: white;
+text-decoration: none;">"The enhancement of your physical condition" -By Omar Clinton</a>
+	<br>
+	<br>
+	<br>
+
+	<b>Athletic History:</b><br>
+	Full Scholarship UNC Chapel Hill<br>
+	Professional Track athlete: Sprint Dynamics<br>
+	Girivoy (Kettlebell) Sport- Snatch, Long Cycle, Jerk.<br>
+	Ballet Dancer<br>
+	<br>
+	<br>
+
+	<b>References:</b><br>
+	Mike Tejada: 919/414.0030<br>
+	Nate Davis 919/928.4892<br>
+	More available upon request<br>
+</p>
+
+
+
+<br>
+<br>
+<br>
+<br>
+---------------------------------------------------------------
+<br>
+<br>
+Omar Clinton<br>
+919.610.0665<br>
+omarclinton@gmail.com<br>
+GitHub:  https://github.com/Oclinton<br>
+<br>
+Education (Focus and major place in italics)<br>
+-Santa Fe College - 2011-2013 Focus: Ballet<br>
+-Hillsborough Community College - 2009-2011 Focus: Ballet<br>
+-UNC Chapel Hill, NC 1996-2001 Major: Communication Studies<br>
+-Aviano American High School, Aviano Italy<br>
+
+
+<br>
+-------------------------------------------------------------------------------------
+<br>
+<br>
+
+Jan 2016 - May 2016 | Stapels<br>
+Technology Sales Associate<br>
+Responsibilities:<br>
+-Build relationships with customers<br>
+-Sold products and services to customers<br>
+-Ensured all operational policies and processes are followed. Remained current on new technologies, products and services<br>
+-Other duties as assigned<br>
+<br>
+2011 -2014 | Studio Athletica Human Enhancement<br>
+Wellness Entrepreneur Personal Trainer<br>
+Responsibilities:<br>
+-Marketing funnel campaign management. 
+-Personal training sales. 
+-Management of the SOP. 
+-Client management, billing, session tracking, and ongoing sales and support. 
+-Social media management. 
+-Management of client physical well being by managing their training. 
+-Was responsible for the sales of my own personal training services. 
+-Created a new program named Ballet Athletique. 
+-Created and choreographed a dance performance that was performed and a dance festival in Jacksonville FL in the spring of 2013 
+-Designed the Studio-Athletica website 
+https://www.facebook.com/studioathletica?ref=bookmarks 
+
+
+2007 - 2013 | Body Isolations Wellness Entrepreneur
+Dance and Wellness - Performer/Teacher 
+-Perform ballet and Latin dance routines at social and artistic events. 
+-Teach body isolations and salsa workshops to dancers of all levels. 
+-Taught basic and advanced salsa instruction. 
+
+
+Software development:
+Languages and Technologies
+HTML, CSS, Java, JavaScript, Jquery, PHP, XML, Intermediate/beginner AngularJS, MySQL, JSON, CRUD (Create Read Update Delete), Ecommerce Website, Payment integration, Membership Systems, Database Management, PHP Mysql integration
+
+
+<br>
+<br>
+---------------------------------------------------------------
+<br>
+<br>
+
+<b>Designer Tools</b><br>
+-Photoshop<br>
+-Illustrator (I'm weak at the pen tool)<br>
+-Familiarity with Adobe's Creative Design Suite<br>
+-Web Development<br>
+-WordPress<br>
+-HTML<br>
+-CSS<br> 
+-Java<br>
+-JavaScript<br>
+-Bootstrap<br>
+-Jquery<br>
+<br>
+
+<b>Backend Languages</b><br>
+-PHP<br>
+-AJAX<br>
+-XML<br>
+-MySQL<br>
+<br>
+
+<br>
+<b>
+Media Queries
+<br>
+Google Charts
+<b>
+
+
+<b>Design - Conceptual Process</b><br>
+UX/UI design Intermediate/beginner<br> 
+Frontend Developer<br>
+XML<br>
+JSON<br> 
+SQL<br>
+<br> 
+
+<b>Sample Project Familiarity</b><br>
+CMS (Content Management System)<br>
+E-Commerce Website Development<br>
+Payment integration using Stripe and PHP<br>
+Membership Systems <br>
+Database Management<br>
+PHP Mysql integration<br>
+Custom PHP Emails<br>
+Form Handling<br>
+-----Using MySql<br>
+-----Using JSON<br>
+-----Using Google Spreadsheet OAuth<br>
+Custom payment systems<br>
+CRUD (Create Read Update Delete)<br> 
+<br>
+
+<b>Deployment</b><br>
+Containerization with Docker<br>
+Intermediate understanding of DevOps <br>
+<br>
+
+<b>Mobile</b><br>
+Basic Android mobile application development<br>
+<br>
+
+<b>Version Control</b><br>
+Git<br>
+<br>
+
+<b>Templating</b><br>
+WordPress<br>
+Jekyll<br>
+<br>
+
+<b>Currently Learning</b><br>
+React<br>
+NodeJS<br>
+React Native<br>
+Machine Learning<br>
+C<br>
+Embedded Systems<br>
+<br>
+
+</div>
+		</div>
+	</main>
+
+	<footer>
+		<div id="footerDiv">
+			<div id="right"></div>
+			<div id="left"></div>
+		</div>
+		<div id="footer2">
+			<div id="right2"></div>
+			<div id="left2"></div>
+		</div>
+	</footer>
+
+</body>
+</html>
+
