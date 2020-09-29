38a11345c02a6367cd6b7723bff4792b55931094
Oclinton <omarclinton@gmail.com>
2020-09-29 12:16:37 -0400

    tutorials commit

---
+++ tutorials.php
@@ -0,0 +1,68 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>Omar Clinton About</title>
+<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Oxygen">
+<link rel ="stylesheet" type = "text/css" href = css/tutorials.css>
+	<style type="text/css"></style>
+</head>
+<body>
+	<header id="headerDiv">
+		<div id="topBan">
+			<div id = "navContent">
+					<ul>
+						<li><a class="memNavItem" href= "portfolio.php">Portfolio</a></li>
+						<li><a class="first after" href= "resume.php">Resume</a></li>
+						<li><a class="first after" href= "omarabout.php">About Me</a></li>
+						<li><a class="navLink3" href= "tutorials.php">Tutorials</a></li>
+						<li><a class=""    href="services.php">Services</a></li>
+						<li><a class="navLink3" href= "contact.php">Contact</a></li>
+					</ul>
+				</div>
+			</div>
+			<div id="thinLine"></div>
+			<div id="headCont">
+					<a href="index.php"><div id="logoDiv" style="font-family: 'Oxygen', sans-serif">OmarClinton.com</div></a>
+			</div>
+		</div>
+	</header>
+	<div id = "extendNav"></div>
+
+
+
+	<main id = "mainDiv" style="background: #424949">
+		<div id = "sampleHolder">
+				<div id = "sampleHeader"></div>
+				<div id = "image">
+					<img id = "omarPic" src="images/omar.jpg">
+				</div>
+				<div id = "aboutText">
+					<p style = "font-family: sans-serif;color: white;">
+					<h2>Tutorials That have helped me</h2>
+					</p>
+				</div>
+				<div id = "picDiv"></div>
+				<div>
+				<!--<iframe width="560" height="315" src="https://www.youtube.com/embed/QPXOgSRBErw" frameborder="0" allowfullscreen></iframe>
+				<iframe width="560" height="315" src="https://www.youtube.com/embed/RMpzhllxWX0" frameborder="0" allowfullscreen></iframe>
+			-->
+				</div>
+			</div>
+		</div>
+	</main>
+
+
+
+<footer>
+		<div id="footerDiv">
+			<div id="right"></div>
+			<div id="left"></div>
+		</div>
+		<div id="footer2">
+			<div id="right2"></div>
+			<div id="left2"></div>
+</footer>
+
+</body>
+</html>
