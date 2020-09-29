955cc12a84727f621d9bf30ebe511b2792b24085
Oclinton <omarclinton@gmail.com>
2020-09-29 13:10:08 -0400

    a commit

---
+++ portfolio.php
@@ -0,0 +1,104 @@
+
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>Omar Clinton Portfolio</title>
+<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Oxygen">
+<link rel = "stylesheet" type = "text/css" href = "css/portfolio2.css">
+	<style type="text/css"></style>
+</head>
+<body>
+	<header id="headerDiv">
+		<div id="topBan">
+			<div id = "navContent">
+				<ul>
+					<li><a class="memNavItem" href= "portfolio.php">Portfolio</a></li>
+					<li><a class="first after" href= "resume.php">Resume</a></li>
+					<li><a class="first after" href= "omarabout.php">About Me</a></li>
+					<li><a class="navLink3" href= "tutorials.php">Tutorials</a></li>
+					<li><a class="navLink3" href= "services.php">Services</a></li>
+					<li><a class="navLink3" href= "contact.php">Contact</a></li>
+				</ul>
+			</div>
+		</div>
+		<div id="thinLine"></div>
+		<div id="headCont">
+			<a href="index.php"><div id="logoDiv" style="font-family: 'Oxygen', sans-serif">OmarClinton.com</div></a>
+		</div>
+		</div>
+	</header>
+	<div id = "extendNav"></div>
+
+	<main id = "mainDiv" style="background: #424949">
+		<div id = "sampleHolder">
+			<div id = "sampleheader"></div>
+			<div id = "portMenus">
+				<div id = "templates">
+					<div class ="subMenuTop"></div>
+					<div class = "description">
+						<p>
+							<b>Templates</b>
+							<br/>
+							<br/>
+							These are just a few examples of templates.  I posted these examples to show that I understand the basic concept of UI design.  Although they are not real websites I can very easily implement the design principles
+						</p>
+					</div>
+					<div class ="subButton">
+							<a class = "buttons" href="templates.html"><button type="button" id = "button">Check Out Templates!</button></a>
+					</div>
+				</div>
+				<div id = "proofs">
+					<div class = "subMenuTop"></div>
+					<div class = "description">
+						<p>
+							<b>Applications</b>
+							<br/>
+							<br/>
+							The applications in this section will be and are created using Angular JQuery, HTML, CSS. The information can easily but submitted to and queried from a database or JSON file.
+						</p>
+					</div>
+					<div class = "subButton">
+							<a class = "buttons" href="applications.html"><button type="button" id = "button">Check Out Applications!</button></a>
+					</div>
+				</div>
+				<div id = "nsfw">
+					<div class = "subMenuTop">
+					</div>
+					<div class = "description">
+						<p>
+							<b>NSFW</b>
+							<br/>
+							<br/>
+							This section is the NSFW section (Not Safe For Work). Some of the material in this section is approprate for individuals 18 years of age or older.  The design templates of this section comporses a lot of different componets.  Login, input validation and sanitization, and payment options as an example.
+						</p>
+					</div>
+					<div class = "subButton">
+						<a class = "buttons" href="nsfw.html"><button type="button" id = "button">Check Out NSFW!</button></a>
+					</div>
+				</div>
+			</div>
+				<!--<a href="indexJ.php" class = "portlink">
+					<div id = "sampleDiv1"></div></a>
+				<a href="viv2.php" class = "portlink">
+					<div id = "sampleDiv2"></div></a>
+				<a href="skye.php" class = "portlink">
+					<div id = "sampleDiv3"></div><a/>
+				<a href="new.html" class = "portlink">
+					<div id = "sampleDiv4"></div><a/>
+				<a href="slidersample.php" class = "portlink">
+					<div id = "sampleDiv5"></div><a/> -->
+		</div>
+	</main>
+	<footer>
+		<div id="footerDiv">
+			<div id="right"></div>
+			<div id="left"></div>
+		</div>
+		<div id="footer2">
+			<div id="right2"></div>
+			<div id="left2"></div>
+		</div>
+	</footer>
+</body>
+</html>
