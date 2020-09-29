b1849a1917be8f6d43d4075aede7734e3ecd9930
Oclinton <omarclinton@gmail.com>
2020-09-29 12:39:31 -0400

    first commit

---
+++ index.php
@@ -0,0 +1,100 @@
+
+
+
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>Omar Clinton Index</title>
+<!--<link rel ="stylesheet" type = "text/css" href = header.css>-->
+<link rel = "stylesheet" type = "text/css" href = "https://fonts.googleapis.com/css?family=Oxygen">
+<link href="https://fonts.googleapis.com/css?family=Libre+Barcode+39+Extended|Libre+Barcode+39+Text" rel="stylesheet">
+<link rel = "stylesheet" type = "text/css" href = "css/index.css">
+<link href="https://fonts.googleapis.com/css?family=Libre+Barcode+39+Extended" rel="stylesheet">
+
+<meta content="width=device-width, initial-scale=1" name="viewport" />
+
+</head>
+<body>
+		<header id = "headerDiv">
+			<div id = "topBan">
+				<img src="images/mobile-menu.png" id="mobile-menu">
+				<div id = "navContent">
+			
+
+					<ul>
+						<li><a class="memNavItem" href= "portfolio.php">Portfolio</a></li>
+						<li><a class="first after" href= "resume.php">Resume</a></li>
+						<li><a class="first after" href= "omarabout.php">About Me</a></li>
+						<li><a class="navLink3" href= "tutorials.php">Tutorials</a></li>
+						<li><a class="navLink3" href= "services.php">Services</a></li>
+						<li><a class="navLink3" href= "contact.php">Contact</a></li>
+					</ul>
+				</div>
+			</div>
+			<div id = "thinLine"></div>
+			<div id = "headCont">
+
+				<!--<div id = "navContent"></div> -->
+
+				<a href = "index.html">
+				<!--	<style>
+					#logoDiv{font-size: 60px;}
+				</style> -->
+					<!--	<div id="logoDiv" style="font-family: 'Libre Barcode 39 Extended', cursive;">OmarClinton.com</div></a> -->
+				 <a href = "index.html"><div id="logoDiv" style="font-family: 'Oxygen', sans-serif">OmarClinton.com</div></a>
+				<!-- <a href = "index.html"><div id="logoDiv" style="font-family: 'Libre Barcode 39 Text', cursive;">OmarClinton.com</div></a> -->
+
+				</div>
+			</div>
+		</header>
+
+<main id = "mainDiv" style="background: #424949">
+		 <div id = "windowMain">
+			 <!-- <div id = "portfolio"></div>
+			<div id = "resume"></div>
+			<div id = "aboutMe"></div>
+			<div id = "contact"></div> -->
+			<div id = "login">
+				<form action = loginreg.php method = POST>
+					<input type = "text" name = "username" class = "login" placeholder ="User Name"><br>
+					<input  type = "password" name = "password" class = "login" placeholder = "Password"><br>
+					<button type = "button" name = "submit" class = "login">Enter</button>
+					<button type = "button" name = "submit" class = "login">Members Beta</button>
+				</form>
+			</div>
+	</div>
+</main>
+
+<footer>
+	<div id = "footerDiv">
+		<div id = "right"></div>
+		<div id = "left"></div>
+	</div>
+	<div id = "footer2">
+		<div id = "right2"></div>
+		<div id = "left2"></div>
+	</div>
+
+</footer>
+
+</body>
+</html>
+
+<?php
+/*
+ echo "hello";
+ echo '<br>';
+ $ipAddress = $_SERVER['REMOTE_ADDR'];
+ $serverAddress = $_SERVER['SERVER_ADDR'];
+ echo "<br>";
+ echo "Server Address: ";
+ echo "<br>";
+// echo $serverAddress;
+ echo "<br>";
+// echo $ipAddress;
+ //MAC address-----
+ //$MAC = exec('getmac'); 
+
+ */
+?>
