1e2a8c0bdda09db52cf5690864d10f5f55001874
Oclinton <omarclinton@gmail.com>
2020-09-29 12:10:34 -0400

    Working on Commits

---
+++ services.php
@@ -0,0 +1,87 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>Omar Clinton About</title>
+<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Oxygen">
+<link rel ="stylesheet" type = "text/css" href = css/omarabout.css>
+	<style type="text/css"></style>
+</head>
+<body>
+	<header id="headerDiv">
+		<div id="topBan">
+			<div id = "navContent">
+					<ul>
+						<li><a class="memNavItem" href= "portfolio.php">Portfolio</a></li>
+						<li><a class="first after" href= "resume.php">Resume</a></li>
+						<li><a class="first after" href= "omarabout.php">About Me</a></li>
+						<li><a class="navLink3" href= "tutorials.php">Tutorials</a></li>
+						
+						<li><a class="navLink3" href= "contact.php">Contact</a></li>
+					</ul>
+				</div>
+			</div>
+			<div id="thinLine"></div>
+			<div id="headCont">
+					<a href="index.php"><div id="logoDiv" style="font-family: 'Oxygen', sans-serif">OmarClinton.com</div></a>
+			</div>
+		</div>
+	</header>
+	<div id = "extendNav"></div>
+
+	<main id = "mainDiv" style="background: #424949">
+		<div id = "sampleHolder">
+			<div id = "sampleHeader"></div>
+			<div id = "image"><img id = "omarPic" src="images/omar.jpg"></div>
+			<div id = "aboutText">
+				<p style = "font-family: sans-serif;color: white;">
+				<h2>About Me</h2>
+				I genuinly enjoy computer programming.<br>
+				I have a good eye for balance.<br>
+				I am interested in computer science from logic gates to web server and database servers.  I'm looking forward to what I will learn and am always striving to improve my understanding of the underlying architechture as well as the abstraction of the instructions given to the machine.<br>
+				<h3>Classes</h3>
+				(All have been taken in the pased 12 months)<br>
+				Principles of OO Programming<br>
+				Data Structures<br>
+				Machine Learning<br>
+				Internet Security<br>
+				Advanced www programming<br>
+				Algorithm Analysis<br>
+				Operating Systems<br>
+				Digital Logic<br>
+				Networking<br>
+				<h3>Stuff I Know How To Do</h3>
+				Input Validation<br>
+				Input Sanitization<br>
+				Work with Hash Functions<br>
+				<h3>Web Site Development</h3>
+				If you are interested in a website I would love to help you.<br>
+				Static Website <br>
+				Dynamic Website<br>
+				Website using server scripting (php)<br>
+				Website using input validation<br>
+				Website using input sanitization<br>
+				</p>
+			</div>
+
+		<div id = "picDiv"></div>
+			<div>
+			<iframe width="560" height="315" src="https://www.youtube.com/embed/QPXOgSRBErw" frameborder="0" allowfullscreen></iframe>
+			<iframe width="560" height="315" src="https://www.youtube.com/embed/RMpzhllxWX0" frameborder="0" allowfullscreen></iframe>
+			</div>
+		</div>
+	</div>
+</main>
+
+<footer>
+		<div id="footerDiv">
+			<div id="right"></div>
+			<div id="left"></div>
+		</div>
+		<div id="footer2">
+			<div id="right2"></div>
+			<div id="left2"></div>
+</footer>
+
+</body>
+</html>
